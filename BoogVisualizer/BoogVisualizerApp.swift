8d60ff0f7dc77ce8c43f90c433e54d3332cc02d8
boogiemonsta <boogiemonsta@gmail.com>
2022-03-26 16:51:31 +0100

    Initial Commit

---
+++ BoogVisualizer/BoogVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoogVisualizerApp.swift
+//  BoogVisualizer
+//
+//  Created by Ahmed D'Ala on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BoogVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoogVisualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoogVisualizer
+//
+//  Created by Ahmed D'Ala on 3/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
