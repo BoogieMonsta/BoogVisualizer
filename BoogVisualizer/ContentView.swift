1ab650b9e43ef92019e2170b1050c3de354b2540
boogiemonsta <boogiemonsta@gmail.com>
2022-03-26 19:42:33 +0100

    wip

--- BoogVisualizer/ContentView.swift
+++ BoogVisualizer/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+let numberOfSamples: Int = 10
+
 struct ContentView: View {
+    @ObservedObject private var mic = MicrophoneMonitor(numberOfSamples: numberOfSamples)
+    
+    private func normalizeSoundLevel(level: Float) -> CGFloat {
+        let level = max(0.2, CGFloat(level) + 50) / 2 // between 0.1 and 25
+        
+        return CGFloat(level * (250 / 25)) // scaled to max at 250 (height of our bar)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                HStack(spacing: 4) {
+                    ForEach(mic.soundSamples, id: \.self) { level in
+                        BarView(value: self.normalizeSoundLevel(level: level))
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BarView: View {
+    var value: CGFloat
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(.white)
+                .frame(width: (UIScreen.main.bounds.width - CGFloat(numberOfSamples) * 4) / CGFloat(numberOfSamples), height: value)
+        }
     }
 }
---
+++ BoogVisualizer/MicrophoneMonitor.swift
@@ -0,0 +1,84 @@
+//
+//  MicrophoneMonitor.swift
+//  BoogVisualizer
+//
+//  Created by Ahmed D'Ala on 3/26/22.
+//
+
+import Foundation
+import AVFoundation
+
+// inherits from ObservableObject
+class MicrophoneMonitor: ObservableObject {
+    
+    private var audioRecorder: AVAudioRecorder
+    private var timer: Timer?
+    
+    private var currentSample: Int
+    private let numberOfSamples: Int
+    
+    // make soundSamples an Observable
+    @Published public var soundSamples: [Float]
+    
+    init(numberOfSamples: Int) {
+        
+        // make sure numberOfSamples is always > 0 to avoid bugs
+        self.numberOfSamples = numberOfSamples > 0 ? numberOfSamples : 1
+        self.soundSamples = [Float](repeating: .zero, count: numberOfSamples)
+        self.currentSample = 0
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        if audioSession.recordPermission != .granted {
+            audioSession.requestRecordPermission { (isGranted) in
+                if !isGranted {
+                    fatalError("Please allow audio recording")
+                }
+            }
+        }
+        
+        // settings to record microphone audio
+        // TODO: change this url so we can save audio recordings
+        let url = URL(fileURLWithPath: "/dev/null", isDirectory: true)
+        let recorderSettings: [String:Any] = [
+            AVFormatIDKey: NSNumber(value: kAudioFormatAppleLossless),
+            AVSampleRateKey: 44100.0,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue
+        ]
+        
+        // necessary to play and record audio on user's phone
+        do {
+            audioRecorder = try AVAudioRecorder(url: url, settings: recorderSettings)
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [])
+            
+            startMonitoring()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    // called as soon as object is initialized
+    private func startMonitoring() {
+        
+        // metering was disabled by default, we enable it track the sound level
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.record()
+        
+        // set time interval to lower value for higher def/more bars in the visualization
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
+            
+            // refresh average and peak values for all channels of the recorder
+            self.audioRecorder.updateMeters()
+            
+            // calculate average power per channel
+            self.soundSamples[self.currentSample] = self.audioRecorder.averagePower(forChannel: 0)
+            self.currentSample = (self.currentSample + 1) % self.numberOfSamples
+        })
+    }
+    
+    // clean up
+    deinit {
+        timer?.invalidate()
+        audioRecorder.stop()
+    }
+}
